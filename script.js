c0538d18323a8169a52bf0e093519df8c7869dfe
MariaDalposso98 <maria.dalposso@escola.pr.gov.br>
2026-04-18 18:21:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 
 const PRODUTOS = [
-    { nome: "Gloss Glambox Pink", preco: "R$ 44,90", img: "https://m.media-amazon.com/images/I/517iC2D-3rL._AC_SL1000_.jpg" },
-    { nome: "Base Real Filter", preco: "R$ 89,90", img: "https://m.media-amazon.com/images/I/51+x-X-e0YL._AC_SL1000_.jpg" },
-    { nome: "Paleta de Sombras Fran", preco: "R$ 119,90", img: "https://m.media-amazon.com/images/I/61kYyV7qT-L._AC_SL1200_.jpg" },
-    { nome: "Pó Solto Invisible", preco: "R$ 68,00", img: "https://m.media-amazon.com/images/I/51C+uBOnH5L._AC_SL1000_.jpg" }
+    { 
+        nome: "Gloss Glambox Pink", 
+        preco: "R$ 44,90", 
+        img: "https://m.media-amazon.com/images/I/517iC2D-3rL._AC_SL1000_.jpg" 
+    },
+    { 
+        nome: "Base Real Filter", 
+        preco: "R$ 89,90", 
+        img: "https://m.media-amazon.com/images/I/51+x-X-e0YL._AC_SL1000_.jpg" 
+    },
+    { 
+        nome: "Paleta de Sombras Fran", 
+        preco: "R$ 119,90", 
+        img: "https://m.media-amazon.com/images/I/61kYyV7qT-L._AC_SL1200_.jpg" 
+    },
+    { 
+        nome: "Pó Solto Invisible", 
+        preco: "R$ 68,00", 
+        img: "https://m.media-amazon.com/images/I/51C+uBOnH5L._AC_SL1000_.jpg" 
+    }
 ];
-
 function render() {
     const grid = document.getElementById('product-grid');
     grid.innerHTML = PRODUTOS.map(p => `
